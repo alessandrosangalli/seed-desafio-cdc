032c9ee3d26edff2e7d73e0ec45b5407502aaa11
Alessandro Sangalli <sangalli.alessandro@gmail.com>
2024-02-09 05:15:55 -0800

    Email can't be empty

---
+++ DesafioDevEficiente/Controllers/AuthorController.fs
@@ -0,0 +1,32 @@
+﻿namespace DesafioDevEficiente.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open AccidentalFish.FSharp.Validation
+open DesafioDevEficiente
+
+type CreateAuthorDto = {
+    email: string
+}
+
+[<ApiController>]
+[<Route("[controller]")>]
+type AuthorController (logger : ILogger<AuthorController>) as this =
+    inherit ControllerBase()
+
+    [<HttpPost>]
+    member _.Post([<FromBody>] createAuthorDto) =
+        let validateAuthor = createValidatorFor<CreateAuthorDto>() {
+            validate(fun o -> o.email) [
+                isNotEmpty
+            ]
+        }
+
+        match validateAuthor createAuthorDto with
+        | Ok -> this.Ok() :> IActionResult
+        | Errors errors -> this.BadRequest(errors) :> IActionResult
+        
